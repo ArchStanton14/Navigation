24c80a2ecc7373a619f748d1e3fd340e1adccfc7
Nathaniel Stanford <nathanieljstanford@gmail.com>
2024-11-16 21:21:19 -0500

    need to develope technique more

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Navigation
 A tool to help navigate indoor spaces
+
+Will read CSV to create a virtual map; use object and nodes to navigate between points within it, and later come up with some kind of graphical representation of this.
+
+Use giant hashmap of rooms and locations to find where to route to
+
+Create nesting template to give routes 
+
+Hall: Buildings have Hall: Floors with Hall: Halls
+Each object tells you how to get to the sub object
+
+
+might wait for chapter on trees for this one
---
+++ hall.hpp
@@ -0,0 +1,29 @@
+//Author: Nathaniel Stanford
+//2024-10-25
+//Software Analysis and Design II
+//Navigation project
+//Implementation first attempt for hall object
+
+//This object will be a template usable for halls, floors, and buildings
+#ifndef HALL_
+#define HALL_
+
+template <class T> 
+class hall{
+    public:
+        struct room{
+            int number; 
+            //instructions to get there
+        }
+
+        findRoute(entrance, room)
+
+    private:
+        std::vector<T> rooms;
+        std::vector
+
+
+};
+
+
+#endif
