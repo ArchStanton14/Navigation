22832bffe0b8017f80d06bb7b4fda4d9998b75c4
Nathaniel Stanford <nathanieljstanford@gmail.com>
2025-01-14 16:46:35 -0500

    Bare declarations

--- Floor.hpp
+++ Floor.hpp
@@ -1,11 +1,30 @@
+#include "Node.hpp"
+#include <string>
+
 /*
 Floor class has nodes and can find routes between them. will eventually read from csv for floorplan, and display graphically
 Since we don't know where we will be starting, we need to store the nodes for access in this class. We can't rely on a starting pointer
 */
+struct Room{    //what our nodes will hold
+    std::string name;
+    double x;   //x coord on map
+    double y;   //y coord on map
+};
+
 class Floor{
     public:
+        Floor();
+        Floor(const std::vector<Node<Room>*>& rooms);
+
+        void setRooms(std::vector<Node<Room>*> rooms);
+        bool addRoom(Node<Room> room);
+        std::vector<Node<Room>*> getRooms() const;
+
+
+        std::vector<Node<Room>*> Djikstra(Node<Room>* start, Node<Room>* destination);    //our pathfinding algorithm. It will return a list of nodes to follow
 
     private:
+        std::vector<Node<Room>*> Rooms;
 
     
-}
+};
--- Node.hpp
+++ Node.hpp
@@ -1,38 +1,39 @@
+#include <vector>
 /*
     Node for naviagation in undirected graph. Must have weight to each connected node
 
 
  */
-
+template<class T>
 struct NW{  //struct to keep track of weights of connections
-    *Node<T>;   //pointer to node
+    Node<T>* node;   //pointer to node
     double weight;  //weight of connection
-}
+};
 
 template<class T>
 class Node{
     public:
       
         Node();
-        Node(T item);
-        Node(T item, vector<NW> connections);
+        Node(const T& item);
+        Node(const T& item, std::vector<NW> connections);
 
         void setItem(const T &item);
 
         T getItem() const;
 
-        void setConnections(const vector<NW>& connections);
+        void setConnections(const std::vector<NW>& connections);
         
-        bool addConnection(const vector<NW>& connections);
+        bool addConnection(const std::vector<NW>& connections);
 
-        bool removeConnection(const vector<NW>& connections);
+        bool removeConnection(const std::vector<NW>& connections);
 
-        vector<NW> getConnections() const;
+        std::vector<NW> getConnections() const;
 
     private:
         T item;
-        vector<NW> connections;
+        std::vector<NW> connections;
 
 
 
-}
+};
